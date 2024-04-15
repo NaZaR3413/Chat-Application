6ba2900e0b7787a130024fc043241d44edf489a0
Nilay Patel <nilayno1@gmail.com>
2024-04-14 22:27:39 -0700

    clienghandler setup

--- Server.java
+++ Server.java
@@ -19,11 +19,11 @@ public class Server {
                 System.out.println("New client connection: " + client.getInetAddress().getHostAddress());
 
                 // create new thread object
-                // implement clienthandler class
-                //ClientHandler clientSocket = new ClientHandler(client);
+                // implement clienthandler class:
+                ClientHandler clientSocket = new ClientHandler(client);
 
                 // thread to handle each client
-                //new Thread(clientSocket).start();
+                new Thread(clientSocket).start();
 
             }
         }
@@ -40,5 +40,55 @@ public class Server {
                 }
             }
         }
+    }
+
+    private static class ClientHandler implements Runnable {
+        private final Socket clientSocket;
+
+        // constructor
+        public ClientHandler(Socket _socket)
+        {
+            this.clientSocket = _socket;
+        }
+
+        public void run() {
+            PrintWriter output = null;
+            BufferedReader input = null;
+
+            try {
+                    // get output of client 
+                    output = new PrintWriter(clientSocket.getOutputStream(), true);
+
+                    // get input of client 
+                    input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+                    String line;
+                    while((line = input.readLine()) != null) {
+                        // print out recieved msg from client
+                        System.out.printf("Sent from client: %s\n", line);
+                        output.print(line);
+                    }
+
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            finally {
+                try {
+                    if(output != null) {
+                        output.close();
+                    }
+                    if(input != null) {
+                        input.close();
+                        clientSocket.close();
+                    }
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }
+
     }
 }
