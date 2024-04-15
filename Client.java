6e19ce686983e8ce51343bea060053a15557382c
Nilay Patel <nilayno1@gmail.com>
2024-04-14 18:23:28 -0700

    established client class

---
+++ Client.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class Client {
+    public static void main(String[] args) {
+
+        // establish connection to port
+        try (Socket socket = new Socket("localhost", 8080)) {
+            // writing to server
+            PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
+
+            // reading from server
+            BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            // set up scanner/reader objects
+            Scanner scan = new Scanner(System.in);
+            String line = null;
+
+            // establish exit as the keyword to end the process
+            while(!"exit".equalsIgnoreCase(line)) {
+                // scan in inputs
+                line = scan.nextLine();
+
+                // send inputs to server side
+                output.println(line);
+                output.flush();
+
+                // print server replies
+                System.out.println("Server replies: " + input.readLine());
+
+            }
+
+            // close scan object when exit
+            scan.close();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
