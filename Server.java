6e0f7d1f7a2bf5da321dd20ab833bbc266ed6eda
Nilay Patel <nilayno1@gmail.com>
2024-04-23 23:39:34 -0700

    congruent thread setups

--- Server.java
+++ Server.java
@@ -1,133 +1,132 @@
 import java.io.*;
-import java.net.*;
 import java.util.*;
-import java.util.concurrent.ConcurrentHashMap;
-
-public class Server {
-    // setup map to keep track of clients
-    private static Map<String, PrintWriter> clientMap = new ConcurrentHashMap<>();
-    public static void main(String[] args) {
-        ServerSocket server = null;
-
-        try {
-            // server is listening on port 8080
-            server = new ServerSocket(8080);
-            server.setReuseAddress(true);
-            
-            // infinite loop to search for client requests
-            while(true) {
-                // accept incoming client requests
-                Socket client = server.accept();
-
-                // display new client that connected to server
-                System.out.println("New client connection: " + client.getInetAddress().getHostAddress());
-
-                // create new thread object
-                // implement clienthandler class:
-                ClientHandler clientSocket = new ClientHandler(client);
-
-                // thread to handle each client
-                new Thread(clientSocket).start();
-
-            }
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-        }
-        finally {
-            if(server != null) {
-                try {
-                    server.close();
-                }
-                catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
+import java.net.*;
+ 
+// Server class
+public class Server 
+{
+ 
+    // Vector to store active clients
+    static Vector<ClientHandler> ar = new Vector<>();
+     
+    // counter for clients
+    static int i = 0;
+ 
+    public static void main(String[] args) throws IOException 
+    {
+        // server is listening on port 1234
+        ServerSocket ss = new ServerSocket(8080);
+         
+        Socket s;
+         
+        // running infinite loop for getting
+        // client request
+        while (true) 
+        {
+            // Accept the incoming request
+            s = ss.accept();
+ 
+            System.out.println("New client request received : " + s);
+             
+            // obtain input and output streams
+            DataInputStream dis = new DataInputStream(s.getInputStream());
+            DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+             
+            System.out.println("Creating a new handler for this client...");
+ 
+            // Create a new handler object for handling this request.
+            ClientHandler mtch = new ClientHandler(s,"client " + i, dis, dos);
+ 
+            // Create a new Thread with this object.
+            Thread t = new Thread(mtch);
+             
+            System.out.println("Adding this client to active client list");
+ 
+            // add this client to active clients list
+            ar.add(mtch);
+ 
+            // start the thread.
+            t.start();
+ 
+            // increment i for new client.
+            // i is used for naming only, and can be replaced
+            // by any naming scheme
+            i++;
+ 
         }
     }
-
-    private static class ClientHandler implements Runnable {
-        private final Socket clientSocket;
-
-        // constructor
-        public ClientHandler(Socket _socket)
+}
+ 
+// ClientHandler class
+class ClientHandler implements Runnable 
+{
+    Scanner scn = new Scanner(System.in);
+    private String name;
+    final DataInputStream dis;
+    final DataOutputStream dos;
+    Socket s;
+    boolean isloggedin;
+     
+    // constructor
+    public ClientHandler(Socket s, String name,
+                            DataInputStream dis, DataOutputStream dos) {
+        this.dis = dis;
+        this.dos = dos;
+        this.name = name;
+        this.s = s;
+        this.isloggedin=true;
+    }
+ 
+    @Override
+    public void run() {
+ 
+        String received;
+        while (true) 
         {
-            this.clientSocket = _socket;
-        }
-
-        public void run() {
-            PrintWriter output = null;
-            BufferedReader input = null;
-            String clientName = null;
-
-            try {
-                    // get output of client 
-                    output = new PrintWriter(clientSocket.getOutputStream(), true);
-
-                    // get input of client 
-                    input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-
-                    clientName = input.readLine();
-                    System.out.println("Client username: " + clientName);
-
-                    // add user to clientMap
-                    synchronized (clientMap) {
-                        clientMap.put(clientName, output);
+            try
+            {
+                // receive the string
+                received = dis.readUTF();
+                 
+                System.out.println(received);
+                 
+                if(received.equals("logout")){
+                    this.isloggedin=false;
+                    this.s.close();
+                    break;
+                }
+                 
+                // break the string into message and recipient part
+                StringTokenizer st = new StringTokenizer(received, "#");
+                String MsgToSend = st.nextToken();
+                String recipient = st.nextToken();
+ 
+                // search for the recipient in the connected devices list.
+                // ar is the vector storing client of active users
+                for (ClientHandler mc : Server.ar) 
+                {
+                    // if the recipient is found, write on its
+                    // output stream
+                    if (mc.name.equals(recipient) && mc.isloggedin==true) 
+                    {
+                        mc.dos.writeUTF(this.name+" : "+MsgToSend);
+                        break;
                     }
-
-                    // string to read each client input
-                    String line;
-                    while((line = input.readLine()) != null) {
-                        // print out recieved msg from client on server side
-                        System.out.println("Sent from " + clientName + ": " + line);
-                        //System.out.printf("Sent from client: %s\n", line);
-                        //output.println(line);
-                        //output.flush();
-                        
-                        // Assume the input format: "recipientName:message"
-                        int separatorIndex = line.indexOf(":");
-
-                        if (separatorIndex != -1) {
-                            // extract intended recipient
-                            String recipient = line.substring(0, separatorIndex);
-                            // extract the message
-                            String message = line.substring(separatorIndex + 1);
-                            PrintWriter recipientOut = clientMap.get(recipient);
-                            if (recipientOut != null) { 
-                                recipientOut.println(clientName + ": " + message);
-                                // flush out 
-                                recipientOut.flush();
-                            } else {
-                                output.println("Recipient not found: " + recipient);
-                                output.flush();
-                            }
-                        }
-                    }
-
-            }
-            catch (IOException e) {
+                }
+            } catch (IOException e) {
+                 
                 e.printStackTrace();
             }
-            finally {
-                // remove client from map if added
-                synchronized (clientMap) {
-                    clientMap.remove(clientName);
-                }
-                try {
-                    if(output != null) {
-                        output.close();
-                    }
-                    if(input != null) {
-                        input.close();
-                        clientSocket.close();
-                    }
-                }
-                catch (IOException e) {
-                    e.printStackTrace();
-                }
-
-            }
+             
         }
-
+        try
+        {
+            // closing resources
+            this.dis.close();
+            this.dos.close();
+             
+        }catch(IOException e){
+            e.printStackTrace();
+        }
     }
 }
