0cdd0c69b45faef8852800c7fb6b5dc35c615e30
Nilay Patel <nilayno1@gmail.com>
2024-05-06 21:37:43 -0700

    sql integration

--- Database.java
+++ Database.java
@@ -2,17 +2,38 @@ import java.sql.*;
 
 public class Database {
     // sql database url
-    private static final String url = "";
-    private static final String user = ""; // mysql username
-    private static final String password = ""; // mysql password
+    private static final String url = "jdbc:mysql://localhost:3306/chatApp";
+    private static final String user = "root"; // mysql username
+    private static final String password = "nhp000r123"; // mysql password
+
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("ERROR: MySQL JDBC Driver not found.");
+            e.printStackTrace();
+        }
+    }
 
     public static Connection connect() {
         //try to connect to the database. return nothing if unsuccessful
         try {
-            return DriverManager.getConnection(url);
+            return DriverManager.getConnection(url, user, password);
         } catch (SQLException e) {
             System.out.println("ERROR: Database Connection Failed: " + e.getMessage());
             return null;
+        }
+    }
+
+    public static void main(String[] args) {
+        try (Connection conn = connect()) {
+            if (conn != null) {
+                System.out.println("Successfully connected to the database!");
+            } else {
+                System.out.println("Failed to connect to the database.");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
     
---
+++ Main.java
@@ -0,0 +1,17 @@
+import java.sql.Connection;
+import java.sql.SQLException;
+
+
+public class Main {
+    public static void main(String[] args) {
+        try (Connection conn = Database.connect()) {
+            if (conn != null) {
+                System.out.println("Successfully connected to the database!");
+            } else {
+                System.out.println("Failed to connect to the database.");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
